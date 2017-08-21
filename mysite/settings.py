29d68516ae7dd5b532cc8abbf6199c7f32b5a301
witflash <witflash@ukr.net>
2017-08-21 18:41:58 +0300

    Changed DATABASE to PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,14 +74,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+LOGIN_REDIRECT_URL = '/'
+
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'witflash',
+        'PASSWORD': 'mas911',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -105,3 +111,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
